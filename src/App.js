a54e6f36e2f7fb51e3e164d8eebfb1d43a40699d
waldomagalona <waldomagalona@gmail.com>
2021-08-28 09:53:24 +0800

    add to-do list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+//TODO:
+// Registration
+// Login
+// Send Message
+// Retrieve Message
+// Create Channel
+// Get all users
+// Get channel details via channel ID
+// Add member to a channel
+// List of All Users
+
+import React from 'react';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
