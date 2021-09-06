7529399683141193673ae64d66a1602b21c97cc2
Ronniel de Ramos <ronnielderamos@gmail.com>
2021-09-06 16:42:29 +0800

    Chat text input component: on going

--- src/components/main/chat/ChatTextInput.jsx
+++ src/components/main/chat/ChatTextInput.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React,{useRef} from "react";
 
 export default function ChatTextInput(){
+
+    const inputEl = useRef("")
+
+    function handleChange(){
+        
+    }
+
     return(
         <textarea
-      {...register("body")}
+        onChange={handleChange}
+        ref={inputEl}
+        value={inputEl.current.value}
         name="body"
         className="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none"
         rows="1"
